b868415aa2b755b9d3b9b0a18547ef6e949486dd
wangkang <wkang2015@163.com>
2018-05-17 15:21:26 +0800

    Field assignment benchmarks with MethodHandle since Java 7

---
+++ struqt-util-benchmark/src/main/java/com/struqt/jmh/MeasureAssignment.java
@@ -0,0 +1,276 @@
+package com.struqt.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Field assignment benchmarks with MethodHandle since Java 7.
+ *
+ * <p>Read more:
+ *
+ * <ul>
+ *   <li><a href="https://jcp.org/en/jsr/detail?id=292">JSR 292</a>
+ *   <li><a href="https://gist.github.com/raphw/881e1745996f9d314ab0">FieldBenchmark.java</a>
+ * </ul>
+ *
+ * <p>Benchmark Test Results:
+ *
+ * <pre>
+ * # JMH version: 1.20
+ * # VM version: JDK 1.8.0_162, VM 25.162-b12
+ * # VM options: none
+ * # Warmup: 5 iterations, 1 s each
+ * # Measurement: 3 iterations, 2 s each
+ * # Threads: 1 thread, will synchronize iterations
+ * # Benchmark mode: Average time, time/op
+ *
+ * Benchmark                             Mode  Cnt   Score    Error  Units
+ * MeasureAssignment._101_DirectField    avgt    3   6.717 ±  1.237  ns/op
+ * MeasureAssignment._102_DirectSetter   avgt    3   6.937 ±  1.293  ns/op
+ * MeasureAssignment._201_InvokeExact    avgt    3   7.138 ±  0.291  ns/op
+ * MeasureAssignment._202_Invoke         avgt    3   7.089 ±  0.796  ns/op
+ * MeasureAssignment._203_InvokeSwitch   avgt    3  14.504 ±  0.953  ns/op
+ * MeasureAssignment._301_ReflectField   avgt    3  30.435 ± 13.630  ns/op
+ * MeasureAssignment._302_ReflectMethod  avgt    3  59.395 ±  8.325  ns/op
+ * </pre>
+ *
+ * @see java.lang.invoke.MethodHandle
+ */
+@State(Scope.Thread)
+public class MeasureAssignment {
+
+  public static void main(String[] args) throws Throwable {
+    new Runner(
+            new OptionsBuilder()
+                .include(MeasureAssignment.class.getSimpleName())
+                .mode(Mode.AverageTime)
+                .timeUnit(TimeUnit.NANOSECONDS)
+                .warmupIterations(5)
+                .warmupTime(TimeValue.seconds(1))
+                .measurementIterations(3)
+                .measurementTime(TimeValue.seconds(2))
+                .forks(1)
+                .build())
+        .run();
+  }
+
+  private static MockData parent = new MockData();
+  private static List<MockData> children = new ArrayList<>(0);
+
+  @Benchmark
+  public MockData _101_DirectField() {
+    MockData data = new MockData();
+    data.boolValue = true;
+    data.intValue = 99;
+    data.doubleValue = 1.23;
+    data.parent = parent;
+    data.children = children;
+    return data;
+  }
+
+  @Benchmark
+  public MockData _102_DirectSetter() {
+    MockData data = new MockData();
+    data.setBoolValue(true);
+    data.setIntValue(99);
+    data.setDoubleValue(1.23);
+    data.setParent(parent);
+    data.setChildren(children);
+    return data;
+  }
+
+  @Benchmark
+  public MockData _201_InvokeExact() throws Throwable {
+    MockData data = new MockData();
+    MockData.Assignment.intValueSetter.invokeExact(data, 99);
+    MockData.Assignment.boolValueSetter.invokeExact(data, true);
+    MockData.Assignment.doubleValueSetter.invokeExact(data, 1.23);
+    MockData.Assignment.parentSetter.invokeExact(data, parent);
+    MockData.Assignment.childrenSetter.invokeExact(data, children);
+    return data;
+  }
+
+  @Benchmark
+  public MockData _202_Invoke() throws Throwable {
+    MockData data = new MockData();
+    MockData.Assignment.intValueSetter.invoke(data, 99);
+    MockData.Assignment.boolValueSetter.invoke(data, true);
+    MockData.Assignment.doubleValueSetter.invoke(data, 1.23);
+    MockData.Assignment.parentSetter.invoke(data, parent);
+    MockData.Assignment.childrenSetter.invoke(data, children);
+    return data;
+  }
+
+  @Benchmark
+  public MockData _203_InvokeSwitch() throws Throwable {
+    MockData data = new MockData();
+    MockData.Assignment.setObject(data, "intValue", 98);
+    MockData.Assignment.setObject(data, "boolValue", true);
+    MockData.Assignment.setObject(data, "doubleValue", 1.23);
+    MockData.Assignment.setObject(data, "parent", parent);
+    MockData.Assignment.setObject(data, "children", children);
+    return data;
+  }
+
+  @Benchmark
+  public MockData _301_ReflectField() throws Throwable {
+    MockData data = new MockData();
+    MockData.Assignment2.intValueSetterF.setInt(data, 97);
+    MockData.Assignment2.boolValueSetterF.setBoolean(data, true);
+    MockData.Assignment2.doubleValueSetterF.setDouble(data, 1.23);
+    MockData.Assignment2.parentSetterF.set(data, parent);
+    MockData.Assignment2.childrenSetterF.set(data, children);
+    return data;
+  }
+
+  @Benchmark
+  public MockData _302_ReflectMethod() throws Throwable {
+    MockData data = new MockData();
+    MockData.Assignment3.intValueSetterM.invoke(data, 99);
+    MockData.Assignment3.boolValueSetterM.invoke(data, true);
+    MockData.Assignment3.doubleValueSetterM.invoke(data, 1.23);
+    MockData.Assignment3.parentSetterM.invoke(data, parent);
+    MockData.Assignment3.childrenSetterM.invoke(data, children);
+    return data;
+  }
+
+  public static class MockData {
+
+    int intValue;
+    boolean boolValue;
+    double doubleValue;
+    MockData parent;
+    List<MockData> children;
+
+    void setIntValue(int intValue) {
+      this.intValue = intValue;
+    }
+
+    void setBoolValue(boolean boolValue) {
+      this.boolValue = boolValue;
+    }
+
+    void setDoubleValue(double doubleValue) {
+      this.doubleValue = doubleValue;
+    }
+
+    void setParent(MockData parent) {
+      this.parent = parent;
+    }
+
+    void setChildren(List<MockData> children) {
+      this.children = children;
+    }
+
+    abstract static class Assignment {
+
+      static void setObject(MockData data, String field, Object value) throws Throwable {
+        int hash = field.hashCode();
+        switch (hash) {
+          case 556050114:
+            intValueSetter.invokeExact(data, (int) value);
+            break;
+          case 2044569767:
+            boolValueSetter.invokeExact(data, (boolean) value);
+            break;
+          case -1626611680:
+            doubleValueSetter.invokeExact(data, (double) value);
+            break;
+          case -995424086:
+            parentSetter.invokeExact(data, (MockData) value);
+            break;
+          case 1659526655:
+            childrenSetter.invokeExact(data, (List) value);
+            break;
+          default:
+            break;
+        }
+      }
+
+      static final MethodHandle intValueSetter;
+      static final MethodHandle boolValueSetter;
+      static final MethodHandle doubleValueSetter;
+      static final MethodHandle parentSetter;
+      static final MethodHandle childrenSetter;
+
+      static final Map<String, Integer> fieldHashCodes = new HashMap<>();
+
+      static {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        try {
+          intValueSetter = lookup.findSetter(MockData.class, "intValue", int.class);
+          boolValueSetter = lookup.findSetter(MockData.class, "boolValue", boolean.class);
+          doubleValueSetter = lookup.findSetter(MockData.class, "doubleValue", double.class);
+          parentSetter = lookup.findSetter(MockData.class, "parent", MockData.class);
+          childrenSetter = lookup.findSetter(MockData.class, "children", List.class);
+
+          fieldHashCodes.put("intValue", "intValue".hashCode());
+          fieldHashCodes.put("boolValue", "boolValue".hashCode());
+          fieldHashCodes.put("doubleValue", "doubleValue".hashCode());
+          fieldHashCodes.put("parent", "parent".hashCode());
+          fieldHashCodes.put("children", "children".hashCode());
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+          throw new IllegalStateException(e);
+        }
+      }
+    }
+
+    static class Assignment2 {
+
+      static final Field intValueSetterF;
+      static final Field boolValueSetterF;
+      static final Field doubleValueSetterF;
+      static final Field parentSetterF;
+      static final Field childrenSetterF;
+
+      static {
+        try {
+          intValueSetterF = MockData.class.getDeclaredField("intValue");
+          boolValueSetterF = MockData.class.getDeclaredField("boolValue");
+          doubleValueSetterF = MockData.class.getDeclaredField("doubleValue");
+          parentSetterF = MockData.class.getDeclaredField("parent");
+          childrenSetterF = MockData.class.getDeclaredField("children");
+        } catch (NoSuchFieldException e) {
+          throw new IllegalStateException(e);
+        }
+      }
+    }
+
+    static class Assignment3 {
+
+      static final Method intValueSetterM;
+      static final Method boolValueSetterM;
+      static final Method doubleValueSetterM;
+      static final Method parentSetterM;
+      static final Method childrenSetterM;
+
+      static {
+        try {
+          intValueSetterM = MockData.class.getDeclaredMethod("setIntValue", int.class);
+          boolValueSetterM = MockData.class.getDeclaredMethod("setBoolValue", boolean.class);
+          doubleValueSetterM = MockData.class.getDeclaredMethod("setDoubleValue", double.class);
+          parentSetterM = MockData.class.getDeclaredMethod("setParent", MockData.class);
+          childrenSetterM = MockData.class.getDeclaredMethod("setChildren", List.class);
+        } catch (NoSuchMethodException e) {
+          throw new IllegalStateException(e);
+        }
+      }
+    }
+  }
+}
